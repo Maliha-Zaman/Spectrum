98ae76deaf344f1ce4276213b4d96cf661904bae
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2023-03-01 19:05:42 +0600

    chatsystem

--- Chat_System/frontend/src/Pages/Homepage.js
+++ Chat_System/frontend/src/Pages/Homepage.js
@@ -35,14 +35,14 @@ function Homepage() {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans">
-          Talk-A-Tive
+          Spectrum
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
         <Tabs isFitted variant="soft-rounded">
           <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
