6771536a846b8c6867e5ae205b72bd1480f9b950
sami <ittehadrahman@iut-dhaka.edu>
2023-03-11 22:22:09 +0600

    prog

--- AuthInMern-Authentication-In-MERN/client/src/App.js
+++ AuthInMern-Authentication-In-MERN/client/src/App.js
@@ -8,6 +8,7 @@ import PasswordReset from "./components/PasswordReset";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Team from "./components/team/Team";
+import Store from "./components/stores/Stores";
 
 import './App.css';
 function App() {
@@ -18,6 +19,7 @@ function App() {
 		<Routes>
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/stores" exact element={<Store />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
 			<Route path="/forgot-password" element={<ForgotPassword />} />
---
+++ AuthInMern-Authentication-In-MERN/client/src/components/stores/Stores.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../common/heading/Header'
+import Footer from '../footer/Footer'
+import "../stores/stores.css"
+const Stores = () => {
+  return (
+    <>
+        <Header />
+        <div className='stores'>
+            
+        </div>
+        <Footer />
+    </>
+  )
+}
+
+export default Stores
