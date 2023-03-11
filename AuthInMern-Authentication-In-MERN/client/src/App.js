6d1be2517c342d5f6f8d401fe5700767ff1595c0
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2023-03-11 21:21:53 +0600

    commit

--- AuthInMern-Authentication-In-MERN/client/src/App.js
+++ AuthInMern-Authentication-In-MERN/client/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
@@ -8,27 +8,26 @@ import PasswordReset from "./components/PasswordReset";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Team from "./components/team/Team";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import "./App.css";
 function App() {
-	const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
 
-	return (
-
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/users/:id/verify/:token" element={<EmailVerify />} />
-			<Route path="/forgot-password" element={<ForgotPassword />} />
-			<Route path="/password-reset/:id/:token" element={<PasswordReset/>} />
-			<Route path="/" exact element={<Home />} />
-			<Route path="/about" exact element={<About />} />
-			<Route path="/team" exact element={<Team />} />
-        	<Route path="/" element={<Navigate replace to="/home" />} />
-		</Routes>
-
-	);
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
+      <Route path="/" exact element={<Home />} />
+      <Route path="/about" exact element={<About />} />
+      <Route path="/team" exact element={<Team />} />
+      <Route path="/" element={<Navigate replace to="/home" />} />
+    </Routes>
+  );
 }
 
 export default App;
--- AuthInMern-Authentication-In-MERN/server/routes/auth.js
+++ AuthInMern-Authentication-In-MERN/server/routes/auth.js
@@ -27,7 +27,7 @@ router.get("/login/failed", (req, res) => {
   });
 });
 
-router.get("/google", passport.authenticate("google", ["profile", "email"]));
+// router.get("/google", passport.authenticate("google", ["profile", "email"]));
 
 // router.get(
 // 	"/google/callback",
