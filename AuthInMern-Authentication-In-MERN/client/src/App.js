616c3ad1572c677f8beea186d9afc848138f7964
sami <ittehadrahman@iut-dhaka.edu>
2023-03-13 10:55:52 +0600

    card

--- AuthInMern-Authentication-In-MERN/client/src/App.js
+++ AuthInMern-Authentication-In-MERN/client/src/App.js
@@ -9,14 +9,13 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Team from "./components/team/Team";
 import Store from "./components/stores/Stores";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 function App() {
   const user = localStorage.getItem("token");
 
 	return (
-
 		<Routes>
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
@@ -30,7 +29,6 @@ function App() {
 			<Route path="/team" exact element={<Team />} />
         	<Route path="/" element={<Navigate replace to="/home" />} />
 		</Routes>
-
 	);
 }
 
--- AuthInMern-Authentication-In-MERN/client/src/components/stores/Stores.jsx
+++ AuthInMern-Authentication-In-MERN/client/src/components/stores/Stores.jsx
@@ -7,7 +7,132 @@ const Stores = () => {
     <>
         <Header />
         <div className='stores'>
-            
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
+          <div className="card_container">
+            <div className="image_container">
+              <img src='' alt="" />
+            </div>
+            <div className="card_content">
+              <div className="card_title">
+                  <h3>Aarong</h3>
+              </div>
+              <div className="card_body">
+                <p>Aarong is one of the most reknowned clothing brand</p>
+              </div>
+            </div>
+            <div className="btn">
+              <button>
+                <a href="">View more</a>
+              </button>
+            </div>
+          </div>
         </div>
         <Footer />
     </>
