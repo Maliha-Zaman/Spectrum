35a22f8c17b23935b6a25f9349cdb439374625d4
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2023-03-14 20:31:49 +0600

    Confirm Password

--- AuthInMern-Authentication-In-MERN/client/src/components/Signup/index.jsx
+++ AuthInMern-Authentication-In-MERN/client/src/components/Signup/index.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
-import{Icon} from 'react-icons-kit';
-import {eyeOff} from 'react-icons-kit/feather/eyeOff';
-import {eye} from 'react-icons-kit/feather/eye'
+import { Icon } from "react-icons-kit";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+import { eye } from "react-icons-kit/feather/eye";
 // import { Input } from "@chakra-ui/react";
 import Header from "../common/heading/Header";
 import { FaChalkboard } from "react-icons/fa";
@@ -48,167 +48,180 @@ const Signup = () => {
     }
   };
 
-  const [type,setType]=useState('password');
-  const [icon,setIcon]=useState(eyeOff);
-   const handleToggle=()=>{    
-    if(type==='password'){
-      setIcon(eye);      
-      setType('text');
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(eye);
+      setType("text");
+    } else {
+      setIcon(eyeOff);
+      setType("password");
     }
-    else{
-      setIcon(eyeOff);     
-      setType('password');
-    }
-  }
-	return (
-		<div className={styles.signup_container}>
-			<div className={styles.signup_form_container}>
-				<div className={styles.left}>
-					<h1>Welcome Back</h1>
-					<Link to="/login">
-						<button type="button" className={styles.white_btn}>
-							Sign in
-						</button>
-					</Link>
-				</div>
-				<div className={styles.right}>
-					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Create Account</h1>
-						<input
-							type="text"
-							placeholder="First Name"
-							name="firstName"
-							onChange={handleChange}
-							value={data.firstName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							name="lastName"
-							onChange={handleChange}
-							value={data.lastName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="email"
-							placeholder="Email"
-							name="email"
-							onChange={handleChange}
-							value={data.email}
-							required
-							className={styles.input}
-						/>
-						
-						
-						<input
-							type={type}
-							placeholder="Password"
-							name="password"
-							onChange={handleChange}
-							value={data.password}
-							required
-							className={styles.input}
-						/>
-						<span onClick={handleToggle}><Icon icon={icon} size={25} /> </span>
-					
-						
-						{error && <div className={styles.error_msg}>{error}</div>}
-						{msg && <div className={styles.success_msg}>{msg}</div>}
-						<button type="submit" className={styles.green_btn}>
-							Sign Up
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+  };
   return (
-    <>
-      <div className={styles.signup_container}>
-        <div className={styles.signup_form_container}>
-          <div className={styles.left}>
-            <h1>Welcome Back</h1>
-            <Link to="/login">
-              <button type="button" className={styles.white_btn}>
-                Sign in
-              </button>
-            </Link>
-            <Link to="/">
-              <button type="button" className={styles.white_btn}>
-                Home
-              </button>
-            </Link>
-          </div>
-          <div className={styles.right}>
-            <form className={styles.form_container} onSubmit={handleSubmit}>
-              <h1>Create Account</h1>
-              <br />
-              <input
-                type="text"
-                placeholder="First Name"
-                name="firstName"
-                onChange={handleChange}
-                value={data.firstName}
-                required
-                className={styles.input}
-              />
-              <input
-                type="text"
-                placeholder="Last Name"
-                name="lastName"
-                onChange={handleChange}
-                value={data.lastName}
-                required
-                className={styles.input}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                onChange={handleChange}
-                value={data.email}
-                required
-                className={styles.input}
-              />
-              <div>
-                <input
-                  type="Password"
-                  placeholder="Password"
-                  name="password"
-                  onChange={handleChange}
-                  value={data.password}
-                  required
-                  className={styles.input}
-                />
-                {/* <input type={passwordShown ? "text" : "password"} /> */}
-                {/* <button onClick={togglePassword}>Show Password</button> */}
-              </div>
-              <input
-                type="Password"
-                placeholder="Confirm Password"
-                name="Confirmpassword"
-                onChange={handleChange}
-                value={data.Confirmpassword}
-                required
-                className={styles.input}
-              />
-              {/*  */}
-              {/*  */}
-              {error && <div className={styles.error_msg}>{error}</div>}
-              {msg && <div className={styles.success_msg}>{msg}</div>}
-              <br />
-              <button type="submit" className={styles.green_btn}>
-                Sign Up
-              </button>
-            </form>
-          </div>
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.left}>
+          <h1>Welcome Back</h1>
+          <Link to="/login">
+            <button type="button" className={styles.white_btn}>
+              Sign in
+            </button>
+          </Link>
+          <Link to="/">
+            <button type="button" className={styles.white_btn}>
+              Home
+            </button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Create Account</h1>
+            <input
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={handleChange}
+              value={data.firstName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={handleChange}
+              value={data.lastName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+
+            <input
+              type={type}
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="Confirmpassword"
+              onChange={handleChange}
+              value={data.Confirmpassword}
+              required
+              className={styles.input}
+            />
+            <span onClick={handleToggle}>
+              <Icon icon={icon} size={25} />{" "}
+            </span>
+
+            {error && <div className={styles.error_msg}>{error}</div>}
+            {msg && <div className={styles.success_msg}>{msg}</div>}
+            <button type="submit" className={styles.green_btn}>
+              Sign Up
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
+  // return (
+  //   <>
+  //     <div className={styles.signup_container}>
+  //       <div className={styles.signup_form_container}>
+  //         <div className={styles.left}>
+  //           <h1>Welcome Back</h1>
+  //           <Link to="/login">
+  //             <button type="button" className={styles.white_btn}>
+  //               Sign in
+  //             </button>
+  //           </Link>
+  //           <Link to="/">
+  //             <button type="button" className={styles.white_btn}>
+  //               Home
+  //             </button>
+  //           </Link>
+  //         </div>
+  //         <div className={styles.right}>
+  //           <form className={styles.form_container} onSubmit={handleSubmit}>
+  //             <h1>Create Account</h1>
+  //             <br />
+  //             <input
+  //               type="text"
+  //               placeholder="First Name"
+  //               name="firstName"
+  //               onChange={handleChange}
+  //               value={data.firstName}
+  //               required
+  //               className={styles.input}
+  //             />
+  //             <input
+  //               type="text"
+  //               placeholder="Last Name"
+  //               name="lastName"
+  //               onChange={handleChange}
+  //               value={data.lastName}
+  //               required
+  //               className={styles.input}
+  //             />
+  //             <input
+  //               type="email"
+  //               placeholder="Email"
+  //               name="email"
+  //               onChange={handleChange}
+  //               value={data.email}
+  //               required
+  //               className={styles.input}
+  //             />
+  //             <div>
+  //               <input
+  //                 type="Password"
+  //                 placeholder="Password"
+  //                 name="password"
+  //                 onChange={handleChange}
+  //                 value={data.password}
+  //                 required
+  //                 className={styles.input}
+  //               />
+  //               {/* <input type={passwordShown ? "text" : "password"} /> */}
+  //               {/* <button onClick={togglePassword}>Show Password</button> */}
+  //             </div>
+  //             <input
+  //               type="Password"
+  //               placeholder="Confirm Password"
+  //               name="Confirmpassword"
+  //               onChange={handleChange}
+  //               value={data.Confirmpassword}
+  //               required
+  //               className={styles.input}
+  //             />
+  //             {/*  */}
+  //             {/*  */}
+  //             {error && <div className={styles.error_msg}>{error}</div>}
+  //             {msg && <div className={styles.success_msg}>{msg}</div>}
+  //             <br />
+  //             <button type="submit" className={styles.green_btn}>
+  //               Sign Up
+  //             </button>
+  //           </form>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default Signup;
