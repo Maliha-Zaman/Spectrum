d486c2cff291e469fd4ae95df3b5c4358885d086
Maliha-Zaman <malihazaman@iut-dhaka.edu>
2023-05-20 17:01:56 +0600

    backend

--- Fiver/api/models/gig.model.js
+++ Fiver/api/models/gig.model.js
@@ -15,6 +15,10 @@ const GigSchema = new Schema(
       type: String,
       required: true,
     },
+    quantity: {
+      type: String ,
+      required:true,
+    },
     totalStars: {
       type: Number,
       default: 0,
