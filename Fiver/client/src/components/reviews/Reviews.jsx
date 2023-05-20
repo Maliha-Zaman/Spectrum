fae4c45808b0e16c7e10a56a9a3638f5b98b7131
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2023-05-20 12:45:57 +0600

    review

--- Fiver/client/src/components/reviews/Reviews.jsx
+++ Fiver/client/src/components/reviews/Reviews.jsx
@@ -1,18 +1,21 @@
-import { useMutation, useQuery,useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import Review from "../review/Review";
 import newRequest from "../../utils/newRequest";
+import { useState } from "react";
 
 import "./Reviews.scss";
-const Reviews = ({gigId}) => {
-   const queryClient = useQueryClient()
+const Reviews = ({ gigId }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const queryClient = useQueryClient();
   const { isLoading, error, data } = useQuery({
     queryKey: ["reviews"],
     queryFn: () =>
       newRequest.get(`/reviews/${gigId}`).then((res) => {
         return res.data;
       }),
-    });
+  });
 
   // const { isLoading, error, data } = useQuery({
   //   queryKey: ["reviews"],
@@ -26,43 +29,58 @@ const Reviews = ({gigId}) => {
     mutationFn: (review) => {
       return newRequest.post("/reviews", review);
     },
-  
-    onSuccess:()=>{
-      queryClient.invalidateQueries(["reviews"])
-    }
-   });
 
-  const handleSubmit = (e) => {
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries(["reviews"]);
+    // },
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const desc = e.target[0].value;
     const star = e.target[1].value;
-    mutation.mutate({ gigId, desc, star });
+    //mutation.mutate({ gigId, desc, star });
+    //
+    try {
+      await mutation.mutateAsync({ gigId, desc, star });
+      // Handle success response
+      //  onSuccess: () => {
+      queryClient.invalidateQueries(["reviews"]);
+      //  },
+    } catch (error) {
+      // Handle error response
+      const errorMessage =
+        error.response?.data?.message || "Something went wrong!";
+      setErrorMessage(errorMessage);
+    }
+    //
   };
 
   return (
-   <div className="reviews">
-              <h2>Reviews</h2>
-              {isLoading
-              ? "loading"
-              : error
-              ? "Something went wrong!"
-              : data.map((review)=><Review key={review._id} review={review}/>)
-              }
-              <div className="add">
-                <h3>Add a review</h3>
-                <form action="" className="addForm" onSubmit={handleSubmit}>
-                  <input type="text" placeholder="write your review"/>
-                <select name="" id="">
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                  <option value={5}>5</option>
-                </select>
-                <button>Send</button>
-                </form>
-              </div>
-            </div>
+    <div className="reviews">
+      <h2>Reviews</h2>
+      {isLoading
+        ? "loading"
+        : error
+        ? "Something went wrong!"
+        : data.map((review) => <Review key={review._id} review={review} />)}
+      <div className="add">
+        <h3>Add a review</h3>
+        <form action="" className="addForm" onSubmit={handleSubmit}>
+          <input type="text" placeholder="write your review" />
+          <select name="" id="">
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          {errorMessage && <div className="error">{errorMessage}</div>}
+
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
