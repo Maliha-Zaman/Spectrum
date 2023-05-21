09154216f4f2b9cadae9aa140ff6ed62a2f38d0d
Maliha-Zaman <malihazaman@iut-dhaka.edu>
2023-05-21 23:38:31 +0600

    all

---
+++ AuthInMern-Authentication-In-MERN/client/public/images/src/components/Signup/index.jsx
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import { Icon } from "react-icons-kit";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+import { eye } from "react-icons-kit/feather/eye";
+import Header from "../common/heading/Header";
+import { FaChalkboard } from "react-icons/fa";
+const Signup = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    Confirmpassword: "",
+  });
+
+  const [error, setError] = useState("");
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  //  const [message, setMessage] = useState("");
+
+  // const [passwordShown, setPasswordShown] = useState(false);
+
+  // // Password toggle handler
+  // const togglePassword = () => {
+  //   // When the handler is invoked
+  //   // inverse the boolean state of passwordShown
+  //   setPasswordShown(!passwordShown);
+  // };
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const url = "http://localhost:8000/api/users";
+      const { data: res } = await axios.post(url, data);
+      setMsg(res.message);
+      setTimeout(() => {
+        setMsg("");
+      }, 5000);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(eye);
+      setType("text");
+    } else {
+      setIcon(eyeOff);
+      setType("password");
+    }
+  };
+  return (
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.left}>
+          <h1>Welcome Back</h1>
+          <Link to="/login">
+            <button type="button" className={styles.white_btn}>
+              Sign in
+            </button>
+          </Link>
+          <Link to="/">
+            <button type="button" className={styles.white_btn}>
+              Home
+            </button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Create Account</h1>
+            <br/>
+            <input
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={handleChange}
+              value={data.firstName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={handleChange}
+              value={data.lastName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+
+            <input
+              type={type}
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            <div className="wrapper">
+              <div className="1">
+              <input
+                  type={type}
+                  placeholder="Confirm Password"
+                  name="Confirmpassword"
+                  onChange={handleChange}
+                  value={data.Confirmpassword}
+                  required
+                  className={styles.input}
+                  />
+              </div>
+              <div className="2">
+{/*     
+                <span onClick={handleToggle}>
+                  <Icon icon={icon} size={15} />
+                </span> */}
+              </div>
+              </div>
+            
+            {error && <div className={styles.error_msg}>{error}</div>}
+            {msg && <div className={styles.success_msg}>{msg}</div>}
+            <button type="submit" className={styles.green_btn}>
+              {/* Sign Up */}
+              {loading ? <>Loading..</> : <>Sign Up</>}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
