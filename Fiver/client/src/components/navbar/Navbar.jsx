f615cfd8e8e4d6f22194e2b4c97853b7a1ea5f53
sami <ittehadrahman@iut-dhaka.edu>
2023-05-21 18:07:05 +0600

    as

--- Fiver/client/src/components/navbar/Navbar.jsx
+++ Fiver/client/src/components/navbar/Navbar.jsx
@@ -42,9 +42,7 @@ function Navbar() {
           {/* <span className="dot">.</span> */}
         </div>
         <div className="links">
-          {/* <span>Liverr Business</span>
-          <span>Explore</span> */}
-          {/* <span>English</span> */}
+
           {
             !currentUser?.isSeller
           }
